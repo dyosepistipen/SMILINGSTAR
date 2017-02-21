0ef5cc75c9e24272c4116511809693bf8bf209b8
Jr Reyes <jr.evilboss@gmail.com>
2017-02-21 18:24:27 +0800

    add new app base

---
+++ application/models/user_model.php
@@ -0,0 +1,78 @@
+<?php
+class User_model extends CI_Model
+{
+	function __construct() 
+	{
+		parent::__construct();
+		$this->load->database("default");
+	}
+	
+	function checkUserId($user_id)
+	{
+		$this->db->where("userid",$user_id);
+		$result = $this->db->get(DBPREFIX."_users");
+		if($result && $result->num_rows()>0)
+		{
+			return TRUE;
+		}
+		else
+		{
+			return FALSE;			
+		}
+	}
+	
+	function addNewUser($data)
+	{
+		if($this->db->insert(DBPREFIX."_users",$data))
+		{
+			return TRUE;
+		}
+		else
+		{
+			return FALSE;
+		}
+	}
+	
+	function getUserByID($user_id)
+	{
+		$this->db->where("id",$user_id);
+		$result = $this->db->get(DBPREFIX."_users");
+		if($result && $result->num_rows()>0)
+		{
+			return $result->result_array();
+		}
+		else
+		{
+			return FALSE;			
+		}
+	}
+	
+	function getUserByEmail($email_id)
+	{
+		$this->db->where("email",$email_id);
+		$result = $this->db->get(DBPREFIX."_users");
+		if($result && $result->num_rows()>0)
+		{
+			return $result->result_array();
+		}
+		else
+		{
+			return FALSE;			
+		}
+	}
+	
+	public function updateUser($user_id,$data)
+	{
+	//	echo "<pre>";print_r($data);die;
+		$this->db->where("id",$user_id);
+		if($this->db->update(DBPREFIX."_users",$data))
+		{
+			return TRUE;
+		}
+		else
+		{
+			return FALSE;
+		}
+	}
+}
+?>
